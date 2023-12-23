fffddb8aba3b1ecdf90f081077263fd8176dbb17
maulairfani <maulairfani15@gmail.com>
2023-12-23 11:10:01 +0700

    visualisasi tag

--- app/Http/Controllers/Admin/InsightController.php
+++ app/Http/Controllers/Admin/InsightController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Language;
 use App\Models\News;
+use App\Models\Tag;
 use App\Models\Insight;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
@@ -17,7 +18,8 @@ class InsightController extends Controller
     public function index()
     {
         $viewsData = News::orderBy('views', 'desc')->take(10)->get();
+        $tagCountsData = Tag::withCount('news')->get();
 
-        return view('admin.insight.index', compact('viewsData'));
+        return view('admin.insight.index', compact('viewsData', 'tagCountsData'));
     }
 }
--- resources/views/admin/insight/index.blade.php
+++ resources/views/admin/insight/index.blade.php
@@ -6,16 +6,18 @@
         <h1>{{ __('admin.Insight') }}</h1>
     </div>
 
-    <div id="container" style="height: 400px;"></div>
+    <div id="container_1" style="height: 400px;"></div>
+    <div id="container_2" style="height: 400px;"></div>
 
     <script>
+        // Bar Chart Views
         var viewsData = @json($viewsData);
 
         var viewsChartData = viewsData.map(function(item) {
             return { name: item.title, y: item.views }
         })
 
-        Highcharts.chart('container', {
+        Highcharts.chart('container_1', {
             chart: {
                 type: 'bar'
             },
@@ -35,6 +37,35 @@
             series: [{
                 name: 'Views',
                 data: viewsChartData
+            }]
+        });
+
+        // Bar Chart Tag Counts
+        var tagCountsData = @json($tagCountsData);
+        var tagCountsChartData = tagCountsData.map(function(item) {
+            return {name: item.name, y:item.news_count}
+        });
+
+        Highcharts.chart('container_2', {
+            chart: {
+                type: 'pie'
+            },
+            title: {
+                text: 'Tag Counts'
+            },
+            xAxis: {
+                categories: tagCountsData.map(function(item) {
+                    return item.name;
+                })
+            },
+            yAxis: {
+                title: {
+                    text: 'Counts'
+                }
+            },
+            series: [{
+                name: 'Counts',
+                data: tagCountsChartData
             }]
         });
     </script>
