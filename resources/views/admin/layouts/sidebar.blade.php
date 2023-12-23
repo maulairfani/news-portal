47ae5275c5c1d378c0dba0cca458620292be1a30
maulairfani <maulairfani15@gmail.com>
2023-12-23 09:15:43 +0700

    save

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -45,7 +45,7 @@
                         <span>{{ __('admin.Advertisement') }}</span></a></li>
             @endif --}}
 
-            {{-- @if (canAccess(['access management index']))
+            @if (canAccess(['access management index']))
                 <li class="dropdown
                 {{ setSidebarActive([
                     'admin.role.*',
@@ -63,7 +63,7 @@
                                 href="{{ route('admin.role.index') }}">{{ __('admin.Roles and Permissions') }}</a></li>
                     </ul>
                 </li>
-            @endif --}}
+            @endif
  
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
